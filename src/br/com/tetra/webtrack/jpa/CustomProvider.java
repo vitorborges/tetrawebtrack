e607a19542f67c7ce9927ee616a19e2bf74e46ac
Carlos Viana <carlos@tetrasolucoes.com.br>
2013-06-13 18:08:24 -0300

    Pessoal, o VRaptor 3.5 sofreu muitas alteracoes, o custom provider agora é o guice, houve uma subdivisao de projetos dentro do VRaptor hibernate entre outros. Por esse motivo, além da pouca documentacao. etornei a versao do projeto para o VRaptor 3.4 com o Custom provider da Spring.

--- src/br/com/tetra/webtrack/jpa/CustomProvider.java
+++ src/br/com/tetra/webtrack/jpa/CustomProvider.java
@@ -1,12 +1,12 @@
 package br.com.tetra.webtrack.jpa;
 
 import br.com.caelum.vraptor.ComponentRegistry;
-import br.com.caelum.vraptor.ioc.guice.GuiceProvider;
+import br.com.caelum.vraptor.ioc.spring.SpringProvider;
 import br.com.caelum.vraptor.util.jpa.EntityManagerCreator;
 import br.com.caelum.vraptor.util.jpa.EntityManagerFactoryCreator;
 import br.com.caelum.vraptor.util.jpa.JPATransactionInterceptor;
 
-public class CustomProvider extends GuiceProvider{
+public class CustomProvider extends SpringProvider{
 
 	@Override
 	protected void registerCustomComponents(ComponentRegistry registry) {
--- src/br/com/tetra/webtrack/jpa/JPAFactory.java
+++ src/br/com/tetra/webtrack/jpa/JPAFactory.java
@@ -10,7 +10,7 @@ public class JPAFactory {
 	 
     public static EntityManager getEntityManager() {
          if(factory == null) {
-              factory = Persistence.createEntityManagerFactory("default");
+              factory = Persistence.createEntityManagerFactory("tetraqi");
          }
          return factory.createEntityManager();
     }
