ee5a94f114eb1a726c7bb0ce41a3647a9379ce46
Carlos Viana <carlos@tetrasolucoes.com.br>
2013-06-17 15:20:37 -0300

    Retornando as alteracoes do Naim

--- src/br/com/tetra/webtrack/controller/SuporteController.java
+++ src/br/com/tetra/webtrack/controller/SuporteController.java
@@ -1,5 +1,6 @@
 package br.com.tetra.webtrack.controller;
 
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.com.tetra.webtrack.session.UserSession;
@@ -16,6 +17,7 @@ public class SuporteController {
     }
     
 
+    @Get("/suporte/tickets")
 	public void tickets(){
 		
 	}
--- src/br/com/tetra/webtrack/model/Ticket.java
+++ src/br/com/tetra/webtrack/model/Ticket.java
@@ -17,5 +17,6 @@ public class Ticket {
 	private long id;
 	@Temporal(TemporalType.TIMESTAMP)
 	private Calendar dtabertura;
+	
 
 }
