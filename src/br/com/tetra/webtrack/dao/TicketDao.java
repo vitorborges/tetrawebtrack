5aa0e66fd364be0ac2124a2db81b27ad22ba988a
Carlos Viana <carlos@tetrasolucoes.com.br>
2013-07-06 11:01:11 -0300

    Commit

--- src/br/com/tetra/webtrack/dao/TicketDao.java
+++ src/br/com/tetra/webtrack/dao/TicketDao.java
@@ -1,41 +1,23 @@
 package br.com.tetra.webtrack.dao;
 
 import java.util.List;
-
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.Query;
-
 import br.com.caelum.vraptor.ioc.Component;
-import br.com.tetra.webtrack.entity.Grupo;
-import br.com.tetra.webtrack.entity.Status;
-import br.com.tetra.webtrack.entity.Ticket;
-import br.com.tetra.webtrack.entity.Usuario;
+import br.com.tetra.webtrack.model.Ticket;
 
 @Component
-public class TicketDao {
+public class TicketDAO {
 
 	protected EntityManager manager;
-
-	public TicketDao() {
-
+	
+	public TicketDAO(EntityManager manager){
 		this.manager = manager;
 	}
-
-	public Ticket TicketPendente() {
-		try {
-			final String STATUS_PENDENTE = "Pendente";
-			Query query = manager.createQuery("SELECT COUNT(t.status) FROM Ticket t where status = :st");
-			query.setParameter("st", STATUS_PENDENTE);
-			return (Ticket) query.getSingleResult();
-		} catch (NoResultException e) {
-			return null;
-		}
+	
+	@SuppressWarnings("unchecked")
+	public List<Ticket> listar() {
+		Query query = manager.createQuery(" from Ticket");
+		return query.getResultList();
 	}
-
-	public Ticket TicketCancelado() {
-		return null;
-
-	}
-
 }
