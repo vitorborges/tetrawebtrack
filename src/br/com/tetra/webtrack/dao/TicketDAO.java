36efa11b8af18d1719c9e26024b9e90311605cdb
Carlos Viana <carlos@tetrasolucoes.com.br>
2013-07-06 19:17:01 -0300

    Acertado imports

--- src/br/com/tetra/webtrack/dao/TicketDAO.java
+++ src/br/com/tetra/webtrack/dao/TicketDAO.java
@@ -1,10 +1,12 @@
 package br.com.tetra.webtrack.dao;
 
 import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+
 import br.com.caelum.vraptor.ioc.Component;
-import br.com.tetra.webtrack.model.Ticket;
+import br.com.tetra.webtrack.entity.Ticket;
 
 @Component
 public class TicketDAO {
